f652324e65af3a26ae2a42157132939b59d078b9
James Aguirre <jamesaguirre98@gmail.com>
2024-04-06 15:33:13 -0700

    formatting

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,30 +3,36 @@ import coloredLogo from "../assets/Mass_Commercial_Cleaning_FINAL-1cREV-SCREEN.s
 export default function Footer() {
   return (
     <footer className="w-full">
-      <div className="p-5 pt-10 px-12 bg-[#005067] text-[#ffffff] w-full flex flex-col sm:flex-row ">
-        <div className="w-1/5">
+      <div className="p-5 pt-10 px-12 bg-[#005067] text-[#ffffff] w-full flex flex-col justify-center lg:flex-row ">
+        <div className="lg:w-1/5 w-full flex justify-center">
           <img className="mt-6" src={coloredLogo}></img>
         </div>
-        <div className="w-1/5 flex-col">
-          <div className="text-lg font-bold mb-4">CONTACT US</div>
-          <div className="text-sm">
-            {`660 Main Street 
 
-            Sturbridge, MA 01566`}
-            <div> 140 Pine Street Florence, MA 01062</div>
-            <div> (413) 568-4696 Info@macommclean.com</div>
+        <div className="lg:w-1/5 flex-col w-full">
+          <div className="text-lg font-bold mb-4 flex justify-center mt-4 lg:mt-auto pb-3">
+            CONTACT US
+          </div>
+          <div className="text-sm">
+            <p className="flex justify-center">660 Main Street </p>
+            <p className="flex justify-center pb-3">Sturbridge, MA 01566</p>
+            <p className="flex justify-center">140 Pine Street</p>
+            <p className="flex justify-center pb-3">Florence, MA 01062</p>
+            <p className="flex justify-center">(413) 568-4696</p>
+            <p className="flex justify-center">Info@macommclean.com</p>
           </div>
         </div>
-        <div className="w-1/5 flex-col">
+
+        <div className="lg:w-1/5 flex-col">
           <div className="text-lg font-bold mb-4">HOURS</div>
           <div>
-            <p className="font-bold">Mon - Fri</p>
+            <p className="font-bold justify-center">Mon - Fri</p>
             <p className="text-sm">8 am - 5pm</p>
           </div>
           <p className="font-bold">Sat & Sun</p>
           <p className="text-sm">Closed</p>
         </div>
-        <div className="w-1/5 flex-col">
+
+        <div className="lg:w-1/5 flex-col">
           <div className="text-lg font-bold mb-4">NAVIGATION</div>
           <div>
             <ul>
@@ -37,6 +43,7 @@ export default function Footer() {
             </ul>
           </div>
         </div>
+
         <div className="w-1/5 flex-col">PRIVACY POLICY</div>
       </div>
     </footer>
