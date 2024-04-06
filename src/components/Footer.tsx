07d641a34b95d36d41c8735df6eb3e38a6dad307
James Aguirre <jamesaguirre98@gmail.com>
2024-04-06 15:52:14 -0700

    navigation section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
 import coloredLogo from "../assets/Mass_Commercial_Cleaning_FINAL-1cREV-SCREEN.svg";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <footer className="w-full">
       <div className="p-5 pt-10 px-12 bg-[#005067] text-[#ffffff] w-full flex flex-col justify-center lg:flex-row ">
-        <div className="lg:w-1/5 w-full flex justify-center">
-          <img className="mt-6" src={coloredLogo}></img>
+        <div className="lg:w-1/5 w-full flex justify-center lg:justify-start">
+          <img className="" src={coloredLogo}></img>
         </div>
 
         <div className="lg:w-1/5 flex-col w-full">
@@ -22,25 +23,35 @@ export default function Footer() {
           </div>
         </div>
 
-        <div className="lg:w-1/5 flex-col">
-          <div className="text-lg font-bold mb-4">HOURS</div>
-          <div>
-            <p className="font-bold justify-center">Mon - Fri</p>
-            <p className="text-sm">8 am - 5pm</p>
+        <div className="lg:w-1/5 flex-col w-full">
+          <div className="text-lg flex justify-center  mt-4 lg:mt-auto pb-3 font-bold">
+            HOURS
           </div>
-          <p className="font-bold">Sat & Sun</p>
-          <p className="text-sm">Closed</p>
+          <div>
+            <p className="font-bold flex justify-center">Mon - Fri</p>
+            <p className="text-sm flex justify-center pb-3">8 am - 5pm</p>
+            <p className="font-bold flex justify-center">Sat & Sun</p>
+            <p className="text-sm flex justify-center">Closed</p>
+          </div>
         </div>
 
-        <div className="lg:w-1/5 flex-col">
-          <div className="text-lg font-bold mb-4">NAVIGATION</div>
+        <div className="lg:w-1/5 flex-col w-full">
+          <div className="text-lg font-bold mb-4 flex justify-center mt-4 lg:mt-auto pb-3">
+            NAVIGATION
+          </div>
           <div>
-            <ul>
-              <li>HOME</li>
-              <li>ABOUT</li>
-              <li>EMPLOYMENT</li>
-              <li>CONTACT</li>
-            </ul>
+            <Link to="/">
+              <p className="flex justify-center text-sm font-bold">HOME</p>
+            </Link>
+            <Link to="/about">
+              <p className="flex justify-center text-sm font-bold">ABOUT</p>
+            </Link>
+            <Link to="/employment">
+              <p className="flex justify-center text-sm font-bold">EMPLOYMENT</p>
+            </Link>
+            <Link to="/contact">
+              <p className="flex justify-center text-sm bont-bold">CONTACT</p>
+            </Link>
           </div>
         </div>
 
